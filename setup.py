d965e432e0bd17b1ff41c324ef9ef97b8c884a36
Arteen M <77122362+KingCasual@users.noreply.github.com>
2021-11-06 16:33:33 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+import cx_Freeze
+import sys
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+build_exe_options = {"packages": ["os", "pygame"]}
+
+cx_Freeze.setup(
+    name="PlatformFighter",
+    version="1.0.7",
+    options={"build_exe": build_exe_options},
+    executables=[cx_Freeze.Executable("Ver_7.py", base=base)]
+)
+
+# python setup.py build
