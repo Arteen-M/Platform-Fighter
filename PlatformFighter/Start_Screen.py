195d32908a60264e99feeaa6f7a6bfb25a059c7d
Arteen M <77122362+KingCasual@users.noreply.github.com>
2021-11-06 16:30:49 -0400

    Update Start_Screen.py

--- PlatformFighter/Start_Screen.py
+++ PlatformFighter/Start_Screen.py
@@ -10,7 +10,13 @@ pygame.mixer.init()
 HEIGHT = 600
 WIDTH = 800
 RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+DARK_BLUE = (0, 0, 200)
+BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+GRAY = (125, 125, 125)
+DARK_GRAY = (125, 125, 150)
 
 FPS = 60
 FramePerSec = pygame.time.Clock()
@@ -25,6 +31,7 @@ def text_objects(text, font, colour):
 
 def startScreen():
     intro = True
+    frame_count = 0
 
     while intro:
         for event in pygame.event.get():
@@ -35,15 +42,17 @@ def startScreen():
                 if event.key == K_SPACE:
                     intro = False
 
-        displaysurface.fill((0, 0, 0))
-        largeText = pygame.font.Font('freesansbold.ttf', 90)
-        smallText = pygame.font.Font("freesansbold.ttf", 45)
-        TextSurf, TextRect = text_objects("Platform Fighter", largeText, RED)  # Rarefield Brawlers
-        smallTextSurf, smallTextRect = text_objects("Press Space to Start", smallText, RED)
-        TextRect.center = ((WIDTH / 2), (HEIGHT / 2))
-        smallTextRect.center = ((WIDTH / 2), (HEIGHT / 2 + 100))
+        displaysurface.fill(GRAY)
+        if frame_count < 60:
+            frame_count += 1
+
+        largeText = pygame.font.SysFont('mingliuextbpmingliuextbmingliuhkscsextb', 90)
+        smallText = pygame.font.SysFont("mingliuextbpmingliuextbmingliuhkscsextb", 45)
+        TextSurf, TextRect = text_objects("Platform Fighter", largeText, BLUE)
+        smallTextSurf, smallTextRect = text_objects("Press Space to Start", smallText, DARK_BLUE)
+        TextRect.center = (((frame_count**2) / 9), (HEIGHT / 2))
+        smallTextRect.center = (WIDTH-((frame_count**2) / 9), (HEIGHT / 2 + 100))
         displaysurface.blit(TextSurf, TextRect)
         displaysurface.blit(smallTextSurf, smallTextRect)
         pygame.display.update()
         FramePerSec.tick(FPS)
-
